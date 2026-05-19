aa9048ae4128bd7531ce48687130c4cf8a80188e
ayanmarvin124 <ayanmarvin124@gmail.com>
2026-05-19 22:58:35 +0530

    ch

--- evaluate.py
+++ evaluate.py
@@ -384,8 +384,8 @@ def tune_policy_on_val(
     -------
     best_threshold, best_bias, val_metrics
     """
-    thresholds = np.round(np.arange(0.10, 0.15, 0.20), 2)
-    biases     = np.round(np.arange(-0.10,-0.05,0.05, 0.10), 2)
+    thresholds = np.round(np.arange(0.15, 0.61, 0.02), 2)
+    biases     = np.round(np.arange(-0.30, 0.31, 0.02), 2)
     best       = None
 
     print("\n" + "=" * 60)
