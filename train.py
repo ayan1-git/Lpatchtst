4c5e2e5dd032610535ce53cfce8ee47c3330041e
ayanmarvin124 <ayanmarvin124@gmail.com>
2026-06-16 15:28:25 +0530

    ch

--- train.py
+++ train.py
@@ -1619,7 +1619,7 @@ def train(file_paths=None):
                 tok=tok,
                 pretrain_end_date=pretrain_end_date,
                 device=device,
-                epochs=getattr(config, "PRETRAIN_EPOCHS", 10),
+                epochs=getattr(config, "PRETRAIN_EPOCHS", 50),
                 max_lr=getattr(config, "PRETRAIN_LR",     5e-5),
             )
         else:
